9e11dc029f03de2ac1c96ab5b80a01031ad5d05b
Anna Jose <joann23july@gmail.com>
2021-10-24 12:32:23 -0400

    initial commit

---
+++ components/MiniProfile.js
@@ -0,0 +1,14 @@
+function MiniProfile() {
+    return (
+        <div className="flex items-center justify-between mt-14 ml-10">
+            <img className="rounded-full border p-[2px] w-16 h-16" src="https://purewows3.imgix.net/images/articles/2021_08/melissa_roxburgh_actress.jpg?auto=format,compress&cs=strip" />
+        <div className="flex-1 mx-4">
+            <h2 className="font-bold">annble</h2>
+            <h3 className="text-sm text-gray-400">welcome to instagram</h3>
+        </div>
+        <button className="text-blue-400 font-semibold">Sign Out</button>
+        </div>
+    )
+}
+
+export default MiniProfile
